841859b52a3473e7f44061fcd941b104d05870dd
Claudius Galo <galoclaudius@icloud.com>
2025-08-06 12:06:31 -0500

    Update team page and add apple icon

--- frontend/app/(root)/team/page.tsx
+++ frontend/app/(root)/team/page.tsx
@@ -7,7 +7,7 @@ export default function Team() {
 		<div className='flex flex-col flex-1 min-h-screen justify-center items-center'>
 			<div className='flex flex-col justify-center items-center gap-5'>
 				<Image
-					src='https://photos.zillowstatic.com/h_l/ISxfi4vquxrksk0000000000.jpg'
+					src='frontend/public/apple-icon.jpg'
 					alt='Elcy'
 					width={100}
 					height={100}
@@ -15,10 +15,10 @@ export default function Team() {
 				/>
 				<div className='flex flex-col p-5'>
 					<h1 className='font-bold text-center text-[16px] md:text-[20px]'>
-						Elcy Pereira, CEO
+						Elcy Pereira, Broker & Realtor
 					</h1>
 					<p className='text-[12px] md:text-[16px]'>
-						Hello, I am here to help you unlock the doors to your future.
+					Elcy is a highly experienced Realtor and licensed Broker who has been serving clients since 2002. Fluent in English, Portuguese, and Spanish, she brings a unique international perspective and a deep understanding of diverse cultures. Known for her patience and personalized approach, Elcy has guided first-time buyers and seasoned investors alike through successful transactions across Northern Virginia and the Shenandoah Valley. Her global clientele and track record of delivering results are a testament to her dedication, expertise, and passion for helping people achieve their real estate goals. Reach out today for a free consultation.
 					</p>
 				</div>
 			</div>
